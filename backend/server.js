f871862e2ea81c799d9688cafdade89fbdc520b4
zdossan <zhanerke.dossan@nu.edu.kz>
2025-08-08 22:13:10 +0500

    Added get,post,delete routes

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,19 @@ app.get("/", (req, res) => {
   res.send("It is working!xxx");
 });
 
+app.get("/api/transactions/:userId", async (req, res) => {
+	try {
+		const { userId } = req.params;
+		
+		const transactions = await sql `SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC`
+		res.status(200).json(transactions) 
+	} catch (error) {
+		console.log("Error getting the transactions", error);
+		res.status(500).json({ message: "Internal server error" });
+	}
+});
+
+
 app.post("/api/transactions", async (req, res) => {
   try {
     const { title, amount, category, user_id } = req.body;
@@ -56,6 +69,27 @@ app.post("/api/transactions", async (req, res) => {
 });
 
 console.log("my port: ", process.env.PORT);
+
+app.delete("/api/transactions/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		if(isNaN(parseInt(id))){
+			return res.status(400).json({ error: "Invalid transaction ID" });
+		}
+		
+		const result = await sql`DELETE FROM transactions WHERE id = ${id} RETURNING *`;
+
+		if (result.length === 0) {
+			return res.status(404).json({ error: "Transaction not found" });
+		}
+
+		res.status(200).json({ message: "Transaction deleted successfully" });
+	} catch (error) {
+		console.log("Error deleting transaction:", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
+});
 
 initDB().then(() => {
   app.listen(PORT, () => {
