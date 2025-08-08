bde49945076ee4850f9174663e11f59845bec371
zdossan <zhanerke.dossan@nu.edu.kz>
2025-08-08 21:23:53 +0500

    Initialized request

--- backend/server.js
+++ backend/server.js
@@ -4,35 +4,62 @@ import { sql } from "./config/db.js";
 dotenv.config();
 
 const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+/*app.use((req, res, next) => {
+	console.log("Hey we hit a req, the method is:", req.method)
+	next()
+})*/
+
 const PORT = process.env.PORT || 5001;
 
-async function initDB(){
-	try{
-		await sql`CREATE TABLE IF NOT EXISTS transactions(
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
 		id SERIAL PRIMARY KEY,
 		user_id VARCHAR(255) NOT NULL,
 		title VARCHAR(255) NOT NULL,
 		amount DECIMAL(10,2) NOT NULL,
 		category VARCHAR(255) NOT NULL,
 		created_at DATE NOT NULL DEFAULT CURRENT_DATE
-	)`
-	console.log("Database initialized successfully");
-	}
-	catch(error){
-		console.error("Error initializing database:", error);
-		process.exit(1); //status code 1 indicates an error,0 indicates success
-	}
+	)`;
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.error("Error initializing database:", error);
+    process.exit(1); //status code 1 indicates an error,0 indicates success
+  }
 }
 
-app.get("/",(req,res)=>{
-	res.send("It's working");
-})
+app.get("/", (req, res) => {
+  res.send("It is working!xxx");
+});
+
+app.post("/api/transactions", async (req, res) => {
+  try {
+    const { title, amount, category, user_id } = req.body;
+
+    if (!title || amount === undefined || !category || !user_id) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+
+    const transaction =
+      await sql`INSERT INTO transactions (user_id, title, amount, category) VALUES (${user_id}, ${title}, ${amount}, ${category})
+			RETURNING *`
+			console.log(transaction)
+			res.status(201).json(transaction[0]);
+  } catch (error) {
+    console.log("Error creating transaction:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 console.log("my port: ", process.env.PORT);
 
 initDB().then(() => {
-	app.listen(PORT,()=>{
-	console.log("Server is running on PORT:", PORT);
-})
-})
+  app.listen(PORT, () => {
+    console.log("Server is running on PORT:", PORT);
+  });
+});
 //get post delete put
