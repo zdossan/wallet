d7c227eaecdb2883876b9cdbf97f70ccffdc04a5
zdossan <zhanerke.dossan@nu.edu.kz>
2025-08-11 14:16:01 +0500

    Added useTransactions hook

--- mobile/my-app/app/(root)/index.jsx
+++ mobile/my-app/app/(root)/index.jsx
@@ -2,10 +2,21 @@ import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
 import { SignOutButton } from '@/components/SignOutButton'
+import { useTransactions } from '../../hooks/useTransactions'
+import {useEffect} from 'react'
 
 export default function Page() {
   const { user } = useUser()
+  const {transactions, summary, isLoading, loadData, deleteTransaction} = useTransactions(user?.id)
 
+  useEffect(()=>{
+    loadData()
+  },[loadData])
+
+  console.log("user:",user.id)
+
+  console.log("Transactions:", transactions)
+  console.log("Summary:", summary)
   return (
     <View>
       <SignedIn>
---
+++ mobile/my-app/hooks/useTransactions.js
@@ -0,0 +1,72 @@
+import {useCallback, useState} from 'react'
+import {Alert} from 'react-native'
+
+const API_URL = 'https://localhost:5001/api'
+
+export const useTransactions = (userId) => {
+	const [transactions, setTransactions] = useState([])
+	const [summary, setSummary] = useState({
+		balance: 0,
+		income: 0,
+		expenses: 0
+	})
+	const [isLoading, setIsLoading] = useState(true)
+
+	//useCallback is used for performance optimization, it will memoize the function
+	const fetchTransactions = useCallback(async()=>{
+		try{
+			const response = await fetch (`${API_URL}/transactions/${userId}`)
+			const data = await response.json()
+			setTransactions(data)
+		}catch(error){
+			console.log('Error fetching transactions:', error)
+		}
+	},[userId])
+
+	const fetchSummary = useCallback(async()=>{
+		try{
+			const reponse = await fetch(`${API_URL}/transactions/summary/${userId}`)
+			const data = await response.json()
+			setSummary(data)
+		}catch(error){
+			console.log('Error fetching summary:', error)
+		}
+	},[userId])
+
+	const loadData = useCallback(async()=>{
+		if(!userId) return
+
+		setIsLoading(true)
+		try{
+			await Promise.all([fetchTransactions(),fetchSummary()])
+		}catch(error){
+			console.error("Error loading data:", error)
+		}finally{
+			setIsLoading(false)
+		}
+	},[fetchTransactions, fetchSummary, userId])
+
+	const deleteTransaction = async(id)=>{
+		try{
+			const response = await fetch(`${API_URL}/transactions/${id}`, {method: 'DELETE'})
+
+			if(!response.ok){
+				throw new Error('Failed to delete transaction')
+			}
+
+			loadData()
+			Alert.alert('Success', 'Transaction deleted successfully')
+		}catch(error){
+			console.error('Error deleting transaction:', error)
+			Alert.alert('Error', error.message)
+		}
+	}
+
+	return {
+		transactions,
+		summary,
+		isLoading,
+		loadData,
+		deleteTransaction
+	}
+}
