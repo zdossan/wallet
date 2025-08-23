4d8c386815da0e356d90d8aa7bc031fe0178ec45
zdossan <zhanerke.dossan@nu.edu.kz>
2025-08-23 15:43:25 +0500

    Added create page

---
+++ mobile/my-app/constants/api.js
@@ -0,0 +1,2 @@
+//const API_URL = 'https://wallet-1-8uzb.onrender.com/api' /*https://wallet-1-8uzb.onrender.com*/
+export const API_URL = 'http://localhost:5001/api'
--- mobile/my-app/hooks/useTransactions.js
+++ mobile/my-app/hooks/useTransactions.js
@@ -1,8 +1,9 @@
 import {useCallback, useState} from 'react'
 import {Alert} from 'react-native'
+import {API_URL} from '../constants/api'
 
-const API_URL = 'https://wallet-1-8uzb.onrender.com'
-
+//const API_URL = 'https://wallet-1-8uzb.onrender.com/api' /*https://wallet-1-8uzb.onrender.com*/
+//const API_URL = 'http://localhost:5001/api'
 export const useTransactions = (userId) => {
 	const [transactions, setTransactions] = useState([])
 	const [summary, setSummary] = useState({
